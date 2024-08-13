8007293574864dd2b191487fbcf751b5de64e4eb
minji <minji85160@gmail.com>
2024-08-13 11:06:08 +0900

    day02react01

--- react/day02/day02react01/src/index.js
+++ react/day02/day02react01/src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const App = () => {
-  return(<>
-    <h3>Welcome to my homepage</h3>
-    <h1>Hello World</h1>
-  </>);
-};
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
