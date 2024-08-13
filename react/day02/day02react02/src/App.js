2aaff8d52785254c18c71e92e7eeed3c8430459f
minji <minji85160@gmail.com>
2024-08-13 15:20:43 +0900

    react day02 todolist 2

--- react/day02/day02react02/src/App.js
+++ react/day02/day02react02/src/App.js
@@ -1,57 +1,17 @@
 import "./App.css"
+import Input from "./components/Input"
+import Output from "./components/Output"
 
 const App = () => {
     return (<div>
         <header className="jumbotron">
             <h1>Todo List</h1>
+            <p>오늘 할 일을 입력하세요</p>
         </header>
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Something clever.." />
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">OK</button>
-                <button class="btn btn-danger" type="button">Cancel</button>
-            </div>
-        </div>
-        <div>
-            <table class="table table-hover">
-                <thead>
-                    <tr style={{ textAlign: "center" }}>
-                        <th>Confirm</th>
-                        <th>Title</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <input type="checkbox"></input>
-                        </td>
-                        <td>미니프로젝트 만들기</td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="checkbox"></input>
-                        </td>
-                        <td>조별 스터디 자료 준비</td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="checkbox"></input>
-                        </td>
-                        <td>마이 홈페이지 만들기</td>
-                        <td>
-                            <button>Delete</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        {/* 입력 기능 */}
+        <Input />
+        {/* 목록 출력 기능 */}
+        <Output />
     </div>);
 };
 
