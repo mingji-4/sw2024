ca5b6a86c10debb08f3b15dd9ec66f2ae130c636
minji <minji85160@gmail.com>
2024-08-14 15:09:39 +0900

    react day02 todolist 주석 추가

--- react/day02/day02react02/src/App.js
+++ react/day02/day02react02/src/App.js
@@ -12,23 +12,24 @@ const App = () => {
 
     const [noCnt, setNoCnt] = React.useState(104);
 
+    // 새로운 할 일을 추가하는 함수
     function appendItem(title) {
-         // setTodoList 함수를 이용해서 데이터 갱신
+        // setTodoList 함수를 이용해서 데이터 갱신
         // 스프리드 연산자 사용하면 편리함
         const newItem = { no: noCnt, title: title, done: false };
         setNoCnt(noCnt + 1);
         setTodoList([...todoListArr, newItem]);
     }
 
+    // 할 일을 삭제하는 함수
     function deleteItem(no) {
         const updatedList = todoListArr.filter(item => item.no !== no);
         setTodoList(updatedList);
     }
 
+    // 할 일을 수정하는 함수
     function updateItem(no, newTitle) {
-        const updatedList = todoListArr.map(item => 
-            item.no === no ? { ...item, title: newTitle } : item
-        );
+        const updatedList = todoListArr.map(item => item.no === no ? { ...item, title: newTitle } : item);
         setTodoList(updatedList);
     }
 
--- react/day02/day02react02/src/components/Output.js
+++ react/day02/day02react02/src/components/Output.js
@@ -17,76 +17,41 @@ export default (props) => {
         setEditNo(null);
     };
 
-    return (
-        <div>
-            <table id="todoListTbl" className="table table-hover">
-                <thead>
-                    <tr style={{ textAlign: "center" }}>
-                        <th>Confirm</th>
-                        <th>Title</th>
-                        <th>Actions</th>
+    return (<div>
+        <table id="todoListTbl" className="table table-hover">
+            <thead>
+                <tr style={{ textAlign: "center" }}>
+                    <th>Confirm</th>
+                    <th>Title</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {todoListArr.map((item, idx) => {
+                    // 중괄호 enter하면 return문을 꼭 넣어야 함
+                    return (<tr key={item.no}>
+                        <td>
+                            <input type="checkbox" />
+                        </td>
+                        <td>
+                            {editNo === item.no ? (
+                                <input value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
+                            ) : (item.title)}
+                        </td>
+                        <td>
+                            {editNo === item.no ? (<>
+                                <button className="btn btn-success" type="button" onClick={() => handleUpdateClick(item.no)}>Save</button>
+                                <button className="btn btn-secondary" type="button" onClick={() => setEditNo(null)}>Cancel</button>
+                            </>) : (<>
+                                <button className="btn btn-primary" type="button" onClick={() => handleEditClick(item)}>Edit</button>
+                                <button className="btn btn-danger" type="button" onClick={() => deleteItem(item.no)}>Delete</button>
+                            </>)}
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    {todoListArr.map((item, idx) => {
-                        // 중괄호 enter하면 return문을 꼭 넣어야 함
-                        return (
-                            <tr key={item.no}>
-                                <td>
-                                    <input type="checkbox" />
-                                </td>
-                                <td>
-                                    {editNo === item.no ? (
-                                        <input
-                                            value={newTitle}
-                                            onChange={(e) => setNewTitle(e.target.value)}
-                                        />
-                                    ) : (
-                                        item.title
-                                    )}
-                                </td>
-                                <td>
-                                    {editNo === item.no ? (
-                                        <>
-                                            <button
-                                                className="btn btn-success"
-                                                type="button"
-                                                onClick={() => handleUpdateClick(item.no)}
-                                            >
-                                                Save
-                                            </button>
-                                            <button
-                                                className="btn btn-secondary"
-                                                type="button"
-                                                onClick={() => setEditNo(null)}
-                                            >
-                                                Cancel
-                                            </button>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <button
-                                                className="btn btn-primary"
-                                                type="button"
-                                                onClick={() => handleEditClick(item)}
-                                            >
-                                                Edit
-                                            </button>
-                                            <button
-                                                className="btn btn-danger"
-                                                type="button"
-                                                onClick={() => deleteItem(item.no)}
-                                            >
-                                                Delete
-                                            </button>
-                                        </>
-                                    )}
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-        </div>
+                    );
+                })}
+            </tbody>
+        </table>
+    </div>
     );
 }
